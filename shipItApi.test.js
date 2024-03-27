ad9d05be4350b339c1831c4e055cad5efcb2c880
Gracee Gallivan <gracee.gallivan@gmail.com>
2024-03-26 17:58:13 -0700

    finished exercise

--- shipItApi.test.js
+++ shipItApi.test.js
@@ -21,17 +21,3 @@ test("Ship Product", async function () {
   });
   expect(res).toEqual(9476);
 });
-
-
-
-
-// test("shipProduct", async function () {
-//   const shipId = await shipProduct({
-//     productId: 1000,
-//     name: "Test Tester",
-//     addr: "100 Test St",
-//     zip: "12345-6789",
-//   });
-
-//   expect(shipId).toEqual(expect.any(Number));
-// });
