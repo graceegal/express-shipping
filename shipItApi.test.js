e9082a9ee574f80e829ec30bfb43262db8ac7f39
Jack Norquist <jack.norquist@outlook.com>
2024-03-26 19:43:59 -0500

    Mockfetch shipit api

--- shipItApi.test.js
+++ shipItApi.test.js
@@ -1,17 +1,37 @@
 "use strict";
 
-const {
-  shipProduct,
-} = require("./shipItApi");
+const fetchMock = require("fetch-mock");
+
+const { shipProduct, SHIPIT_SHIP_URL } = require("./shipItApi");
 
 
-test("shipProduct", async function () {
-  const shipId = await shipProduct({
+
+test("Ship Product", async function () {
+  fetchMock.post(SHIPIT_SHIP_URL, {
+    receipt: {
+      "shipId": 9476
+    },
+    status: 200
+  });
+  const res = await shipProduct({
     productId: 1000,
     name: "Test Tester",
     addr: "100 Test St",
     zip: "12345-6789",
   });
-
-  expect(shipId).toEqual(expect.any(Number));
+  expect(res).toEqual(9476);
 });
+
+
+
+
+// test("shipProduct", async function () {
+//   const shipId = await shipProduct({
+//     productId: 1000,
+//     name: "Test Tester",
+//     addr: "100 Test St",
+//     zip: "12345-6789",
+//   });
+
+//   expect(shipId).toEqual(expect.any(Number));
+// });
